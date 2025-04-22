892c6093865f01a31a65ca9a2873e700d5a175a3
Cesmac M Camp02 Lab04 <cmcp2lab4@academico.cesmac.edu.br>
2025-04-22 20:08:28 -0300

    Ajustes do MenuEditar

--- Estoque/MenuEstoque.py
+++ Estoque/MenuEstoque.py
@@ -10,22 +10,25 @@ def menuEstoque():
         print("\n1. Cadastrar produtos\n2. Ver estoque\n3. Excluir produto\n4. Editar estoque\n5. Sair\n")
         try:
             escolha2 = int(input("Digite um número: "))
-            if escolha2 == 1:
-                produtos(estoque)
-            elif escolha2 == 2:
-                if not estoque:
-                    print("Estoque vazio.")
-                    continue
-                print("\n======================== PRODUTOS NO ESTOQUE ========================\n")
-                for nome, info in estoque.items():
-                    print(f"| Nome: {nome:<20}Preco: R${info['preco']:<10.2f}Quantidade: {info['quantidade']:<10}|\n")
-                print("\n=====================================================================\n")
-            elif escolha2 == 3:
-                RemoverProduto(estoque)
-            elif escolha2 == 4:
-                editar(estoque)
+            if escolha2 in [1,2,3,4,5]:
+                if escolha2 == 1:
+                    produtos(estoque)
+                elif escolha2 == 2:
+                    if not estoque:
+                        print("Estoque vazio.")
+                        continue
+                    print("\n======================== PRODUTOS NO ESTOQUE ========================\n")
+                    for nome, info in estoque.items():
+                        print(f"| Nome: {nome:<20}Preco: R${info['preco']:<10.2f}Quantidade: {info['quantidade']:<10}|\n")
+                    print("\n=====================================================================\n")
+                elif escolha2 == 3:
+                    RemoverProduto(estoque)
+                elif escolha2 == 4:
+                    editar(estoque)
+                else:
+                    print("Sessão finalizada.")
+                    break
             else:
-                print("Sessão finalizada.")
-                break
+                print("Valor inválido. Digite um valor do menu.")
         except ValueError:
             print("Digite um valor válido.")
